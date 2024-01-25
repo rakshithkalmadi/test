3ee9bc3d93242ea61aa88bde37b3b0d17350757b
rakshithkalmadi <rakshithkalmadi@gmail.com>
2024-01-25 17:11:47 +0530

    New

---
+++ app.py
@@ -0,0 +1,10 @@
+# Import the streamlit library
+import streamlit as st
+
+# Use the st.title function to set the title of the app
+st.title('Welcome to my Streamlit App!')
+
+# Use the st.write function to display some text
+st.write('Hello, Streamlit user! This is a simple Streamlit application.')
+
+# Run the app with the command: streamlit run app.py
